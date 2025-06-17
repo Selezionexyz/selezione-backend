04da47b0ae194f8c05954e21988f9f47ba3fdf7e
Selezionexyz <selezionexyzweb@gmail.com>
2025-06-17 21:00:40 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // --------------------- IMPORTS ---------------------
 const express = require('express');
+const bodyParser = require('body-parser');
 const axios = require('axios');
 const cors = require('cors');
 require('dotenv').config();
